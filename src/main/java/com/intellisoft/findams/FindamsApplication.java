6de5517117c946300f9f22b0dccf742708ae704e
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-09-21 16:35:34 +0300

    First commit

---
+++ src/main/java/com/intellisoft/findams/FindamsApplication.java
@@ -0,0 +1,13 @@
+package com.intellisoft.findams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intellisoft/findams/FindamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellisoft.findams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
