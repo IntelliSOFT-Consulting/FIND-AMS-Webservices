ca23af92bca3f40e04c4ecf88f2c9e4981f1a6cf
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-10-26 12:57:03 +0300

    Addition of data store API logic, code refactor

--- src/main/java/com/intellisoft/findams/constants/Constants.java
+++ src/main/java/com/intellisoft/findams/constants/Constants.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public class Constants {
 
     public static final String WHONET_FILE_PATH = "whonet/";
-
     public static final String PROCESSED_FILES_PATH="processed/";
+    public static final  String FIND_AMS_TRACKED_ENTITY_TYPE_ID="sf54V4IRUD8";
+    public static final String FIND_AMS_ORG_UNIT = "YgHyNJOU8hs";
 }
