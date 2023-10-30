47a2ed5c34438e1e70edca30338e739e5685780b
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-10-30 10:31:24 +0300

    Update maven publish

--- src/main/java/com/intellisoft/findams/service/HttpClientService.java
+++ src/main/java/com/intellisoft/findams/service/HttpClientService.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.http.HttpStatus;
 import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
