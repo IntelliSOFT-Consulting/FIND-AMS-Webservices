91fcc9122ee9cb651e1dd2dad1195df86181a114
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-11-14 09:24:09 +0300

    Custom file upload REST contoller

--- src/main/java/com/intellisoft/findams/constants/Constants.java
+++ src/main/java/com/intellisoft/findams/constants/Constants.java
@@ -7,32 +7,32 @@ public class Constants {
 
     public static final String WHONET_FILE_PATH = "whonet/";
     public static final String PROCESSED_FILES_PATH = "processed/";
-    public static final String FIND_AMS_TRACKED_ENTITY_TYPE_ID = "sf54V4IRUD8";
-    public static final String FIND_AMS_ORG_UNIT = "YgHyNJOU8hs";
+    public static final String FIND_AMS_TRACKED_ENTITY_TYPE_ID = "JySrDBa5jo9";
+    public static final String FIND_AMS_ORG_UNIT = "p3FIxnPMytB";
 
     //AMU:
-    public static final String AGE = "cMy7BCuG2JA";
-    public static final String GENDER = "uG1Kyap8SOR";
-    public static final String PATIENT_IP_OP_NO = "EBwtHcuIfBV";
-    public static final String PATIENT_DIAGNOSIS_TENTATIVE_CONFIRMATORY_DX = "ORDOvPTmmEI";
+    public static final String AGE = "UDhKo8Gkvhf";
+    public static final String GENDER = "yWSH12auzll";
+    public static final String PATIENT_IP_OP_NO = "qm3sLorGhAm";
+    public static final String PATIENT_DIAGNOSIS_TENTATIVE_CONFIRMATORY_DX = "bHgyVemP5vZ";
     public static final String ANTIBIOTIC_PRESCRIPTION = "f350NJhsXJ6";
-    public static final String ANTIBIOTIC = "EpSvMp2TuHf";
-    public static final String CATEGORY = "YAl7plGjc6z";
-    public static final String CLASS = "XbPO7DP5Apy";
-    public static final String PROGRAM = "soRzOeG3SPy";
-    public static final String PROGRAM_STAGE_ID = "cVk7JmeR8Ud";
+    public static final String ANTIBIOTIC = "tbRd9RMZfqO";
+    public static final String CATEGORY = "i9FSMauzfg6";
+    public static final String CLASS = "wZJ3FsN182P";
+    public static final String PROGRAM = "el15zrnA8FH";
+    public static final String PROGRAM_STAGE_ID = "el15zrnA8FH";
     public static final String ORG_UNIT = "YgHyNJOU8hs";
 
     // AMC:
-    public static final String DEPARTMENT = "Wr0mguNO8Wh";
+    public static final String DEPARTMENT = "H8xK5fnXmdl";
     public static final String UNIQUE_PRODUCT_IDENTIFIER = "cjH73ajEoxu";
     public static final String PRODUCT_NAME = "gE4cjbsZPEX";
     public static final String PHARMACEUTICAL_FORMULATION = "D0hTrXHaA2d";
-    public static final String DOSAGE_FORM = "DVFF2aCHCOl";
-    public static final String STRENGTH = "k1xxeIIdTj0";
-    public static final String NUMBER_OF_PACKAGES_BEING_DISPENSED = "MHGdssNVYsS";
-    public static final String DAILY_NUMBER_OF_ADMISSIONS = "Af6N1pC6WvC";
-    public static final String DATE_BEING_DISPENSED = "ImtYUnVV9sd";
-    public static final String AMC_PROGRAM = "NFT0bTb7tP1";
-    public static final String AMC_PROGRAM_STAGE_ID = "Z59rx6pEO6V";
+    public static final String DOSAGE_FORM = "w9bEv0jg4AM";
+    public static final String STRENGTH = "mP1RfKBULVk";
+    public static final String NUMBER_OF_PACKAGES_BEING_DISPENSED = "f98dbrHjafq";
+    public static final String DAILY_NUMBER_OF_ADMISSIONS = "qT3LyaFbbae";
+    public static final String DATE_BEING_DISPENSED = "xyfKcCPVDgv";
+    public static final String AMC_PROGRAM = "xHWVrcfpJll";
+    public static final String AMC_PROGRAM_STAGE_ID = "Gtksmia6KVX";
 }
---
+++ src/main/java/com/intellisoft/findams/controller/FileImportController.java
@@ -0,0 +1,33 @@
+package com.intellisoft.findams.controller;
+
+import com.intellisoft.findams.service.FileParsingService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import reactor.core.Disposable;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping(value = "/api/find-ams/file-import", produces = "application/json")
+public class FileImportController {
+    private final FileParsingService parsingService;
+
+    public FileImportController(FileParsingService parsingService) {
+        this.parsingService = parsingService;
+    }
+
+    @PostMapping("/parse-file")
+    public ResponseEntity<Disposable> parseFile(@RequestParam("fileContent") MultipartFile file) throws IOException {
+
+        String filePath = "/Users/nelsonkimaiga/Documents/IntelliSoft/findams/whonet/test_data 16.58.12.txt";
+
+        String fileContent = new String(file.getBytes(), StandardCharsets.UTF_8);
+        return ResponseEntity.status(HttpStatus.OK).body(parsingService.parseFile(filePath, fileContent));
+    }
+}
